53d793b0fc15176b5da182bebf243f5b547e3f16
Pavel <kolomiietspavlo881@gmail.com>
2025-01-12 12:47:58 +0200

    v4: styling changes

--- todo.client/src/app/services/task.service.ts
+++ todo.client/src/app/services/task.service.ts
@@ -1,12 +1,13 @@
 import { Task } from './../models/task-model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private url = 'https://localhost:7219/api/Tasks/';
+  private url = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
---
+++ todo.client/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://localhost:7219/api/Tasks/',
+};
